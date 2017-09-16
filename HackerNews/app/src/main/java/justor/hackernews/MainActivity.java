431b77a4ca7851827649691403a7b141864fc043
Justorkovva <j.mackos@wp.pl>
2017-09-16 14:50:09 +0200

    OC project
    almost all of it is from Open Classrom course,  because my code didn't work :/
    Now it's my turn to make it better

--- HackerNews/app/src/main/java/justor/hackernews/MainActivity.java
+++ HackerNews/app/src/main/java/justor/hackernews/MainActivity.java
@@ -1,39 +1,56 @@
 package justor.hackernews;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.ProgressBar;
-
 import com.oc.hnapp.android.HNQueryTask;
 
 public class MainActivity extends AppCompatActivity {
 
-    private HNQueryTask task = null;
+    private HNQueryTask _task = null;
+
+    private int _page = 0;
+
+    private HNArticlesAdapter _adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final RecyclerView rv = (RecyclerView) findViewById(R.id.list);
-        rv.setLayoutManager(new LinearLayoutManager(this));
-        MyAdapter adapter= new MyAdapter();
-        rv.setAdapter(adapter);
-        task=new HNQueryTask(adapter, 80, 1);
-        task.execute();
+
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        _adapter = new HNArticlesAdapter(this);
+        recyclerView.setAdapter(_adapter);
+
+        loadNext();
+
         final ProgressBar progress = (ProgressBar) findViewById(R.id.progress);
-        adapter.registerAdapterDataObserver(new RecyclerView.AdapterDataObserver() {
+        _adapter.registerAdapterDataObserver(new RecyclerView.AdapterDataObserver() {
             @Override
             public void onChanged() {
                 progress.setVisibility(View.GONE);
             }
         });
+
+    }
+
+    public void loadNext() {
+        if (_task != null && _task.getStatus() != AsyncTask.Status.FINISHED)
+            return ;
+
+        _task = new HNQueryTask(_adapter, 80, ++_page);
+        _task.execute();
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        task.cancel(true);
+        _task.cancel(true);
     }
 }
