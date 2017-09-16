5c37e11146b3b11e3174bae7b8dd5800220235a5
Justorkovva <j.mackos@wp.pl>
2017-09-16 19:16:41 +0200

    other links can open in app
    changed min. API and added webviewclient

--- HackerNews/app/src/main/java/justor/hackernews/Article.java
+++ HackerNews/app/src/main/java/justor/hackernews/Article.java
@@ -5,7 +5,9 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
@@ -24,6 +26,15 @@ public class Article extends AppCompatActivity {
         textView.setText(title);
         WebView myWebView = (WebView) findViewById(R.id.webView);
         myWebView.loadUrl(url);
+        myWebView.getSettings().setJavaScriptEnabled(true);
+
+        myWebView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                view.loadUrl(request.getUrl().toString());
+                return false;
+            }
+        });
 
         final ProgressBar progress = (ProgressBar) findViewById(R.id.progress2);
         HNArticlesAdapter _adapter=new HNArticlesAdapter();
